3f8ddd64654e85e7f3f6f3f1ac6276eaaeb044a0
gusouz4 <gusouza.1@hotmail.com>
2020-01-14 13:28:33 -0300

    template literals

--- main.js
+++ main.js
@@ -1,52 +1,5 @@
-// REST
+const nome = 'Gustavo';
+const idade = 20;
 
-const usuario = {
-    nome: 'Gustavo',
-    idade: 20,
-    empresa: 'Montreal'
-};
-
-const {nome, ...resto} = usuario;
-
-console.log(nome);
-console.log(resto);
-
-const arr = [1,2,3,4];
-
-const [ a, b, ...c ] = arr;
-
-console.log(a);
-console.log(b);
-console.log(c);
-
-function soma(...params){
-    return params.reduce((total, next) => total + next);
-}
-
-console.log(soma(1,3,4));
-
-function teste(a, b, ...params){
-    return params;
-}
-
-console.log(teste(1, 2, 3, 4, 5, 6, 7));
-
-// SPREAD
-
-const arr1 = [1, 2, 3];
-
-const arr2 = [4, 5, 6];
-
-const arr3 = [...arr1, ...arr2];
-
-console.log(arr3);
-
-const usuario1 = {
-    nome: 'Diego',
-    idade: '20',
-    empresa: 'Montreal',
-};
-
-const usuario2 = { ...usuario1, nome:'Gustavo'};
-
-console.log(usuario2);
+//console.log('Meu nome é '+ nome + ' e tenho ' + idade + ' anos');
+console.log(`Meu nome é ${nome} e tenho ${idade} anos`);
