30cb9a54b0cb4e27f8ff4f1e426feedf4d3c2f00
gusouz4 <gusouza.1@hotmail.com>
2020-01-15 13:22:10 -0300

    adição dos arquivos do modulo2

---
+++ modulo2/bundle.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var nome = 'Gustavo';
+var idade = 20;
+var usuario = {
+  nome: nome,
+  idade: idade,
+  empresa: 'Montreal'
+};
+console.log(usuario);
---
+++ modulo2/main.js
@@ -0,0 +1,10 @@
+const nome = 'Gustavo';
+const idade = 20;
+
+const usuario = {
+    nome,
+    idade,
+    empresa: 'Montreal',
+};
+
+console.log(usuario);
